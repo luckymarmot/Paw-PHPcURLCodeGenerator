39e9d49a6febebed714fc6bf0d791f46c9f670e6
Micha Mazaheri <micha@mazaheri.me>
2018-04-23 20:43:56 +0200

    Add genStr to use single or double quotes

--- src/PHPcURLCodeGenerator.js
+++ src/PHPcURLCodeGenerator.js
@@ -3,7 +3,20 @@ import Mustache from 'mustache'
 
 import template_request from '../templates/request.php'
 
-const addslashes = str => (`${str}`).replace(/[\\"]/g, '\\$&')
+const addslashes = str => str.replace(/[\\"]/g, '\\$&')
+
+const escapeSimpleQuotes = str => str.replace(/[\\']/g, '\\$&')
+const escapeDoubleQuotes = str => str.replace(/[\\"]/g, '\\$&')
+
+const genStr = str => {
+    // if contains $, use simple quotes to prevent variable interpolation
+    // if does not contain ' or contains ", use simple quotes too
+    if (str.indexOf('\'') < 0 || str.indexOf('"') >= 0 || str.indexOf('$') >= 0) {
+        return `'${escapeSimpleQuotes(str)}'`
+    }
+    // otherwise, use double quotes
+    return `"${escapeDoubleQuotes(str)}"`
+}
 
 // 
 // URL
@@ -35,46 +48,6 @@ const getHeadersString = request => {
 // Body
 // 
 
-const getJsonBodyObject = (object, indent = 0) => {
-    // null
-    if (object === null) {
-        return null
-    }
-    // string
-    else if ('string' === typeof object) {
-        return `\"${addslashes(object)}\"`
-    }
-    // number
-    else if ('number' === typeof object) {
-        return object
-    }
-    // boolean
-    else if ('boolean' === typeof object) {
-        return object ? 'true' : 'false'
-    }
-    // array
-    else if ('object' === typeof object && object.length != null) {
-        const indent_str = Array(indent + 1).join('  ')
-        const indent_str_children = Array(indent + 2).join('  ')
-        const lines = object.map(value => `${ indent_str_children }${ getJsonBodyObject(value, indent+1) }`)
-        return `[\n${ lines.join(',\n') }\n${ indent_str }]`
-    }
-    // object
-    else if ('object' === typeof object) {
-        const indent_str = Array(indent + 1).join('  ')
-        const indent_str_children = Array(indent + 2).join('  ')
-        const lines = Object.keys(object).map(key => `${ indent_str_children }${ getJsonBodyObject(key) } => ${ getJsonBodyObject(object[key], indent+1) }`)
-        return `[\n${ lines.join(',\n') }\n${ indent_str }]`
-    }
-    return null
-}
-
-const getJsonBodyString = jsonBody => {
-    return Mustache.render(require('../templates/body_json.php'), {
-        json_body_object: getJsonBodyObject(jsonBody)
-    })
-}
-
 const getUrlEncodedBodyString = urlEncodedParams => {
     const params_strings = Object.keys(urlEncodedParams).map(name => ({
         name: addslashes(name),
@@ -103,6 +76,46 @@ const getRawBodyString = rawBody => {
 
 const getLongBodyString = () => {
     return Mustache.render(require('../templates/body_long.php'))
+}
+
+const getJsonBodyString = jsonBody => {
+    return Mustache.render(require('../templates/body_json.php'), {
+        json_body_object: getJsonBodyObject(jsonBody)
+    })
+}
+
+const getJsonBodyObject = (object, indent = 0) => {
+    // null
+    if (object === null) {
+        return null
+    }
+    // string
+    else if ('string' === typeof object) {
+        return genStr(object)
+    }
+    // number
+    else if ('number' === typeof object) {
+        return object
+    }
+    // boolean
+    else if ('boolean' === typeof object) {
+        return object ? 'true' : 'false'
+    }
+    // array
+    else if ('object' === typeof object && object.length != null) {
+        const indent_str = Array(indent + 1).join('  ')
+        const indent_str_children = Array(indent + 2).join('  ')
+        const lines = object.map(value => `${ indent_str_children }${ getJsonBodyObject(value, indent+1) }`)
+        return `[\n${ lines.join(',\n') }\n${ indent_str }]`
+    }
+    // object
+    else if ('object' === typeof object) {
+        const indent_str = Array(indent + 1).join('  ')
+        const indent_str_children = Array(indent + 2).join('  ')
+        const lines = Object.keys(object).map(key => `${ indent_str_children }${ getJsonBodyObject(key) } => ${ getJsonBodyObject(object[key], indent+1) }`)
+        return `[\n${ lines.join(',\n') }\n${ indent_str }]`
+    }
+    return null
 }
 
 const getBodyString = request => {
